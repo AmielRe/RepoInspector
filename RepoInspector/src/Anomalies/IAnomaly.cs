3d2100fb533f545925046411cf9f73f7203ef373
AmielRe <amieli1999@gmail.com>
2023-11-06 20:59:04 +0200

    Create anomaly interface and abstract class.

---
+++ RepoInspector/src/Anomalies/IAnomaly.cs
@@ -0,0 +1,16 @@
+﻿using Smee.IO.Client.Dto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepoInspector.src.Anomalies
+{
+    interface IAnomaly
+    {
+        public bool IsSuspicious(SmeeEvent payload);
+
+        public void Act();
+
+        public void Run(SmeeEvent payload);
+    }
+}
