1ec552c2c460249deb5807d863bf03ac623cf371
AmielRe <amieli1999@gmail.com>
2023-11-06 22:11:31 +0200

    Add abstract property for event name.
    Every anomaly will implement the correct event name and it will be checked against the payload.

--- RepoInspector/src/Anomalies/BaseAnomaly.cs
+++ RepoInspector/src/Anomalies/BaseAnomaly.cs
@@ -7,12 +7,18 @@ namespace RepoInspector.src.Anomalies
 {
     abstract class BaseAnomaly : IAnomaly
     {
+        public abstract string EventName { get; }
+
         public abstract void Act();
 
         public abstract bool IsSuspicious(SmeeEvent payload);
 
         public void Run(SmeeEvent payload)
         {
+            if (!payload.Data.Headers.TryGetValue("x-github-event", out string payloadEvent)) return;
+
+            if (!string.Equals(payloadEvent, EventName)) return;
+
             if(IsSuspicious(payload))
             {
                 Act();
